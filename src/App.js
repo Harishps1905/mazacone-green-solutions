22c9836159a79d6e3849b72571726df623f81420
Harish Swaminathan <harishswami62@gmail.com>
2022-03-19 12:24:53 +0530

    sample commit

--- src/App.js
+++ src/App.js
@@ -9,29 +9,34 @@ function demoAsyncCall() {
 class App extends Component {
   state = {
     loading: true,
+    bgAnime: false,
   };
 
   componentDidMount() {
     // this simulates an async action, after which the component will render the content
-    demoAsyncCall().then(() => this.setState({ loading: false }));
+    demoAsyncCall().then(() =>
+      this.setState({ loading: false, bgAnime: true })
+    );
   }
 
   render() {
-    const { loading } = this.state;
+    const { loading, bgAnime } = this.state;
 
     if (loading) {
       // if your component doesn't have to wait for an async action, remove this block
       return (
-        <div className="App">
+        <div className="AppLoader">
           <Loader />
         </div>
       ); // render null when app is not ready
     }
 
     return (
-      <div className="App">
+      <>
+        <div className={bgAnime ? "App" : "AppLoader"}></div>
+
         <h1 className="font-face-gm">hello world</h1>
-      </div>
+      </>
     );
   }
 }
